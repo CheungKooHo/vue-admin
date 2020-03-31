0d357e941bfe73a1ad6d8343c4ef30bde8c49780
cheungkooho <577873656@qq.com>
2020-03-31 17:25:20 +0800

    ms

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,15 @@ import VueRouter from "vue-router";
 import store from "../store/index"; // 引入状态管理
 import NProgress from "nprogress"; // 引入进度条组件 npm install nprogress --save
 import "nprogress/nprogress.css";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: Home
+  // }
 ];
 
 /**
@@ -112,5 +102,18 @@ const router = new VueRouter({
     }
   ]
 });
+
+//判断是否存在token
+router.beforeEach((to,from,next)=>{
+  NProgress.start()
+  if (to.path !== '/login' && !store.state.token) {
+     next('/login')     //跳转登录
+     NProgress.done()   // 结束Progress
+  }
+  next()
+})
+router.afterEach(() => {
+  NProgress.done() // 结束Progress
+})
 
 export default router;
