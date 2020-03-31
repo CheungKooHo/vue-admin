449e64c1af1b16d96d4190a5f5741ab6a3c01640
cheungkooho <577873656@qq.com>
2020-03-31 17:47:31 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import state from "./state";
+import mutations from "./mutations";
+import actions from "./action";
+import getters from "./getters";
+// 引入vuex数据持久化插件
+import createPersistedState from "vuex-persistedstate";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state,
+  mutations,
+  actions,
+  getters,
+  plugins: [createPersistedState]
+});
