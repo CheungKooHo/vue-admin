d049d7e2925746a728a368084a0650f8476cb14a
cheungkooho <577873656@qq.com>
2020-04-03 15:06:22 +0800

    users组件添加

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -8,13 +8,13 @@
     </el-header>
     <el-container>
       <el-aside width="200px">
-        <el-menu unique-opened="false">
+        <el-menu :router="true" :unique-opened="true">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>用户管理</span>
             </template>
-            <el-menu-item index="1-1">
+            <el-menu-item index="users">
               <i class="el-icon-edit"></i>
               <span>用户列表</span>
             </el-menu-item>
@@ -73,7 +73,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
